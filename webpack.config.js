4a48cc32cb8a2473e9fe912ce5cb54bfcea1f5d6
Maks Zhukov <maks_zhukov_97@mail.ru>
2020-06-18 21:32:05 +0300

    initial config app

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: ["./src/index.js"],
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "[name].[hash].bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, "src"),
+                loader: "babel-loader",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+        new webpack.DefinePlugin({
+            CANVAS_RENDERER: JSON.stringify(true),
+            WEBGL_RENDERER: JSON.stringify(true),
+        }),
+    ],
+};
